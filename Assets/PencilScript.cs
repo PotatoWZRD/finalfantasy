f9c952b700476518b3f135710c1fccb54f87df4b
Adam Siegel <adam.siegel@mymail.champlain.edu>
2025-11-07 22:14:18 -0500

    testpush

---
+++ Assets/PencilScript.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public enum TypeOfTower
+{
+    Pencil,
+    Calculator,
+    Protractor
+}
+
+public class PencilScript : MonoBehaviour
+{
+
+    public GameObject pencil;
+    public TypeOfTower towerType;
+
+
+    public float test;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Time goes down. When it reaches zero, attack.
+    /*
+     
+     
+     */
+
+
+}
