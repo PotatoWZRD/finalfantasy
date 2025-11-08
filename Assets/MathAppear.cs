983524d54ee5597d23f382442cf14f16ef99f063
Jingles5 <jonathan.ingles@mymail.champlain.edu>
2025-11-07 23:17:03 -0500

    pim

--- Assets/MathAppear.cs
+++ Assets/MathAppear.cs
@@ -2,23 +2,45 @@ using UnityEngine;
 
 public class MathAppear : MonoBehaviour
 {
+    public float startTimer = 10;
+    public float currentTime = 0;
+    public GameObject yippee;
+
     [SerializeField] GameObject connectedMath;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        yippee.SetActive(false);
         connectedMath.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!connectedMath.activeSelf)
+        {
+            currentTime += Time.deltaTime;
+        }
+        if (currentTime > startTimer)
+        {
+            yippee.SetActive(true);
+        }
+
     }
 
     private void OnMouseDown()
     {
-        Debug.Log("clicked");
-        connectedMath.SetActive(true);
-        connectedMath.GetComponentInChildren<MathProblem>().test = true;
+        if (yippee.activeSelf)
+        {
+            yippee.SetActive(false);
+            Debug.Log("clicked");
+            connectedMath.SetActive(true);
+            connectedMath.GetComponentInChildren<MathProblem>().test = true;
+            currentTime = 0;
+        }
+        else 
+        {
+            Debug.Log("Chud alert");
+        }
     }
 }
