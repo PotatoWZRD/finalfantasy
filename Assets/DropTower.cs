1bdb454b1c80f93bdad77030b4c7bea184216975
Adam Siegel <adam.siegel@mymail.champlain.edu>
2025-11-07 22:47:52 -0500

    pencil hover

--- Assets/DropTower.cs
+++ Assets/DropTower.cs
@@ -13,13 +13,18 @@ public class DropTower : MonoBehaviour
     public GameObject pencil;
     public TypeOfTower towerType;
 
-
+    public Color alpha;
     public float test;
     Vector3 mousePos;
+
+    private GameObject mouseCursor;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         towerType = TypeOfTower.Pencil;
+
+        mouseCursor = Instantiate(pencil);
+        mouseCursor.GetComponentInChildren<SpriteRenderer>().color = alpha;
     }
 
     // Update is called once per frame
@@ -29,11 +34,15 @@ public class DropTower : MonoBehaviour
         Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePos);
         mouseWorldPosition.z = 0f;
 
+        mouseCursor.transform.position = mouseWorldPosition;
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(pencil, mouseWorldPosition, Quaternion.identity);
+            Instantiate(pencil, mouseWorldPosition, mouseCursor.transform.rotation);
         }
-
+        if (Input.GetMouseButtonDown(1)) 
+        {
+            mouseCursor.transform.Rotate(0, 0, 90);
+        }
     }
 
     //Time goes down. When it reaches zero, attack.
